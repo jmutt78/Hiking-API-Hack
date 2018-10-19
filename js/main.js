2ef9565765745468b1060229b2a8c135cba2f001
jmutt78 <jmcintosh@docusites.com>
2018-10-19 13:51:01 -0700

    Added Style

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@ const googleKey = 'AIzaSyAlfZawjUjjPhFVgPP55skv22vktzNhPGM';
 const googleUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
 const hikeURL = 'https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10';
 
-
-
-
 //formats the params for the hiking api
 function formatQueryParamsHiking(params) {
   const queryItems = Object.keys(params)
@@ -17,68 +14,72 @@ function formatQueryParamsHiking(params) {
 
 //dispalys the results from the hiking api
 function displayResults(responseJson) {
-  console.log(responseJson);
-      $('#results-list').empty();
-
-    for (let i = 0; i < responseJson.trails.length; i++){
-
+  $('#results-list').empty();
+  $('#no-results-list').empty();
+  $('#search-bar').trigger("reset");
 
 
+  if (responseJson.trails.length != 0) {
 
-
+    for (let i = 0; i < responseJson.trails.length; i++) {
       $('#results-list').append(
-        `<ul><a href='${responseJson.trails[i].url}'>${responseJson.trails[i].name}</a>
-        <p>${responseJson.trails[i].summary}</p>
-        <img src='${responseJson.trails[i].imgSmallMed}' alt='${responseJson.trails[i].name} photo'>
-        <br>
-        <h3>Trail Detail</h3>
-        <li>Location: ${responseJson.trails[i].location}</li>
-        <li>Length: ${responseJson.trails[i].length}</li>
-        <li>Condition: ${responseJson.trails[i].conditionStatus}</li>
-        <li>Max Altitude: ${responseJson.trails[i].high}</li>
-        <li>Min Altitude: ${responseJson.trails[i].low}</li>
-        <li>Difficulty: ${responseJson.trails[i].difficulty}</li>
-
-
-        </ul>`
-      )};
-    //display the results section
-    $('#results').removeClass('hidden');
-
+        `<div class="card mb-3">
+  <h3 class="card-header">${responseJson.trails[i].name}r</h3>
+  <div class="card-body">
+    <h5 class="card-title">${responseJson.trails[i].location}</h5>
+  </div>
+ <img src='${responseJson.trails[i].imgSmallMed}' onerror="this.style.display='none'" alt='${responseJson.trails[i].name} photo'>
+  <div class="card-body">
+    <p class="card-text">${responseJson.trails[i].summary}</p>
+  </div>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item">Length: ${responseJson.trails[i].length} Miles</li>
+        <li class="list-group-item">Condition: ${responseJson.trails[i].conditionStatus}</li>
+        <li class="list-group-item">Max Altitude: ${responseJson.trails[i].high}ft</li>
+        <li class="list-group-item" style="color: #ffa707; font-weight: bold; background-color: ${responseJson.trails[i].difficulty};”>Min Altitude: ${responseJson.trails[i].low}</li>
+        <li class="list-group-item">Difficulty: ${responseJson.trails[i].difficulty}</li>
+        </ul>
+  <div class="card-body">
+     <a href='${responseJson.trails[i].url}' target="_blank">Learn More</a>
+  </div>`
+      )
+    };
+  } else {
+    $('#no-results-list').append(`<h3>Sorry! No Results Found</h3>`)
+  }
+  //display the results section
+  $('#results').removeClass('hidden');
 }
 
 //Get request to the hiking api
 function getHikeingData(googleLat, googleLng, minMile, maxMile) {
-  console.log(minMile);
-  console.log(googleLng);
-  console.log(googleLat);
   const params = {
     lat: googleLat,
     lon: googleLng,
-    maxDistance: 10,
+    maxDistance: 15,
+    maxResults: 50,
     minLength: minMile,
     key: hikeKey
-
   };
   const queryString = formatQueryParamsHiking(params)
-      const url = hikeURL + '?' + queryString;
-      console.log(url);
-      fetch(url)
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          }
-          throw new Error(response.statusText);
-        })
-        .then(responseJson => displayResults(responseJson))
-        .catch(err => {
-          $('#js-error-message').text(`Something went wrong: ${err.message}`);
-        });
+  const url = hikeURL + '?' + queryString;
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
 }
 
 
 //handles the slider, the form, converts user city to lat and long,
 function handleGoogle() {
+
   //display the slider
   let sliderElement = $("#slider-range");
   $(function() {
@@ -108,7 +109,7 @@ function handleGoogle() {
         return googleQueryItems.join('&');
       }
 
-      //converts address to lat and long4
+      //converts address to lat and long
       function convertAddress(addressQuary) {
         const googleParams = {
           address: addressQuary,
@@ -140,6 +141,11 @@ function handleGoogle() {
         }
       }
     });
+  }
+  //rest the slider
+  function resetSlider() {
+    var $slider = $("#slider-range");
+    $slider.slider("values", 0, initialMinimumValue);
   }
 }
 
